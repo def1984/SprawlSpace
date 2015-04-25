7492a3b643576c4585e019a3d53ab9535108798f
def1984 <rubydef1984@gmail.com>
2015-04-25 21:40:58 +0800

    new

---
+++ app/src/main/java/com/example/ai/sprawlspace/ui/BaseActivity.java
@@ -0,0 +1,37 @@
+package com.example.ai.sprawlspace.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.ai.sprawlspace.R;
+
+
+public class BaseActivity extends ActionBarActivity {
+    // Primary toolbar and drawer toggle
+    private Toolbar mActionBarToolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        getActionBarToolbar();
+    }
+
+
+    protected Toolbar getActionBarToolbar() {
+        if (mActionBarToolbar == null) {
+            mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+            if (mActionBarToolbar != null) {
+                setSupportActionBar(mActionBarToolbar);
+            }
+        }
+        return mActionBarToolbar;
+    }
+}
